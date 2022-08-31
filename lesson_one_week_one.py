8d4a8411c849060dcda7fa47c5b0c8ef60982b2c
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2022-08-30 21:59:57 -0600

    Update lesson_one_week_one.py

--- lesson_one_week_one.py
+++ lesson_one_week_one.py
@@ -1,1 +1,7 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
 
+# Store the voltage and current data as column vectors.
+I = np.array([[0.2, 0.3, 0.4, 0.5, 0.6]]).T
+V = np.array([[1.23, 1.38, 2.06, 2.47, 3.17]]).T
